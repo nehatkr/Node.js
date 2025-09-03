0d54b9a5bee476022e0ec9eaade6939293840e89
Nehatkr <nehathakur2308@gmail.com>
2025-09-03 09:43:52 +0530

    Day5

--- routes-example.js
+++ routes-example.js
@@ -40,6 +40,47 @@ app.get("/products", (req,res)=>{
 })
 
 
+
+// get a single product
+app.get('/products/:id', (req,res)=>{
+    const productId = parseInt(req.params.id);
+        const products= [
+        {
+            id:1,
+            label:'Product 1'
+        },
+        {
+            id:2,
+            label:'Product 2'
+        },
+        {
+            id:3,
+            label:'Product 3'
+        },
+        {
+            id: 4,
+            label:'Product  4'
+        },
+        {
+            id:5,
+            label:'Product 5'
+        },
+        {
+            id:6,
+            label:'Product 6'
+        },
+     
+    ];
+
+    const getSingleProduct = products.find(product=> product.id === productId);
+
+    if(getSingleProduct){
+        res.json(getSingleProduct)
+    }else{
+        res.status(404).send('Product not found ! please try with different id')
+        
+    }
+})
 const port = 3000;
 app.listen(port, ()=>{
     console.log(`Server is now running at port ${port}`);
